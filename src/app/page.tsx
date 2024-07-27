fe7e5088e30799ce6e6c8aab2530ef7b37d7b036
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-07-27 09:58:26 -0400

    Escaped apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,14 @@ export default function Home() {
       <div className="grid gap-8 place-items-center">
         <p>
           Welcome to Dungeon Forged S&C, where your fitness goals become
-          reality. Here, we don't just offer strength and conditioning—we ignite
-          your potential and fuel your transformation. Coach Sutton is committed
-          to pushing you beyond your limits, crafting bespoke workout plans that
-          align with your ambitions. With a hardcore facility and an energizing
-          atmosphere, every training is designed to challenge, motivate, and
-          elevate you. Join us at Dungeon Forged S&C and discover a team where
-          your dedication turns into success. Embrace the journey, be dungeon
-          forged, and rise to your best self with us.
+          reality. Here, we don&apos;t just offer strength and conditioning—we
+          ignite your potential and fuel your transformation. Coach Sutton is
+          committed to pushing you beyond your limits, crafting bespoke workout
+          plans that align with your ambitions. With a hardcore facility and an
+          energizing atmosphere, every training is designed to challenge,
+          motivate, and elevate you. Join us at Dungeon Forged S&C and discover
+          a team where your dedication turns into success. Embrace the journey,
+          be dungeon forged, and rise to your best self with us.
         </p>
       </div>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:lg:h-[360px]"></div>
