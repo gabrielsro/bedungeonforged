88e103ba4ba38ddf904897717a0d78f941f6021a
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-07-27 17:29:09 -0400

    Refactored home videos into component HomeVideos

---
+++ src/app/_components/homeVideos.tsx
@@ -0,0 +1,66 @@
+export default function HomeVideos() {
+  return (
+    <div className="grid place-content-center">
+      <div className="flex gap-3 py-10 pl-4 pr-10 bg-black lg:gap-8 overflow-x-auto w-screen lg:w-lg-screen-video-container lg:rounded-lg lg:px-20">
+        <iframe
+          src="https://www.youtube.com/embed/9-dsAhoaUnI"
+          title="July 27, 2024"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="aspect-[9/16] w-sm-short lg:w-lg-short"
+        />
+        <iframe
+          src="https://www.youtube.com/embed/seKOtG0tNvg"
+          title="Football Training 2"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="aspect-[9/16] w-sm-short lg:w-lg-short"
+        />
+        <iframe
+          src="https://www.youtube.com/embed/KAAIEkvp9l8"
+          title="softball agility"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="aspect-[9/16] w-sm-short lg:w-lg-short"
+        />
+
+        <iframe
+          src="https://www.youtube.com/embed/9ayvWHhe6iw"
+          title="Football Training"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="aspect-[9/16] w-sm-short lg:w-lg-short"
+        />
+        <iframe
+          src="https://www.youtube.com/embed/k3o9nC_pXh4"
+          title="Dungeon Yoga"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="aspect-[9/16] w-sm-short lg:w-lg-short"
+        />
+        <iframe
+          src="https://www.youtube.com/embed/rN8apxvYdeA"
+          title="Finisher"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="aspect-[9/16] w-sm-short lg:w-lg-short"
+        />
+        <iframe
+          width="860"
+          height="484"
+          src="https://www.youtube.com/embed/89sV3fYqdM8"
+          title="strength training 2"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="aspect-video w-sm-short lg:w-lg-short"
+        />
+      </div>
+    </div>
+  );
+}
